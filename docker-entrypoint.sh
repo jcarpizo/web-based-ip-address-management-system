e93fd09162d6fe91761f8d8638128998a7f6d5f4
Jasper Carpizo <jasper@incube8.sg>
2025-06-20 09:13:04 +0800

    feat: fix issues in env files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
+until mysqladmin ping -h"$DB_HOST" -P"$DB_PORT" --silent; do
+  echo "Waiting for DB at $DB_HOST:$DB_PORT..."
+  sleep 3
+done
+
 cd /var/www
 
 rsync -a --delete "/src/${SERVICE_PATH}/" ./
 
+if [ -f composer.json ]; then
+  echo "→ Installing PHP dependencies with Composer…"
+  composer install --no-interaction --optimize-autoloader
+fi
+
 if [ ! -f .env ]; then
   cp .env.example .env
   php artisan key:generate
+   if [ "$SERVICE_PATH" = "auth-service" ]; then
+      echo "→ Generating JWT secrets and certificates…"
+      php artisan jwt:secret
+      php artisan jwt:generate-certs
+  fi
 fi
 
 until php artisan migrate:fresh --seed --force; do
@@ -15,14 +30,14 @@ until php artisan migrate:fresh --seed --force; do
   sleep 3
 done
 
-# run application tests
-echo "→ Running PHPUnit tests…"
-php artisan test
-
-if [ "$SERVICE_PATH" = "web-client" ]; then
-  echo "→ Installing JS dependencies & building assets for web-client…"
+if [ -f package.json ]; then
+  echo "→ Installing JS dependencies…"
   npm install
   npm run build
 fi
+
+# run application tests
+echo "→ Running PHPUnit tests…"
+php artisan test
 
 php artisan serve --host=0.0.0.0 --port=${APP_PORT}
